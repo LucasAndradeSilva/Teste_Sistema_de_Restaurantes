6fd09b120aca66c07206fc02b5741f2b034ac18a
LucasAndradeSilva <lucasandradesilva2019@gmail.com>
2020-02-28 16:08:23 -0300

    Primeiro Commit

---
+++ system_resturante/src/test/java/com/fiorde/system_resturante/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiorde.system_resturante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
